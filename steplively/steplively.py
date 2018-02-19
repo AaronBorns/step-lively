30eee627a6719e4b6a5ca98cf7a1ff552f3af89a
Aaron Borns <aborns@outlook.com>
2018-02-18 21:08:30 -0500

    Restructured code execution
    setup.py - upticked the version and removed some metadata
    
    steplively.py - restructured execution to only run as __main__

--- steplively/steplively.py
+++ steplively/steplively.py
@@ -14,9 +14,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import plot
-import parse
-
 def warranty():
     print(parse.get_uncommented_text('./assets/warranty.txt'))
 
@@ -30,15 +27,18 @@ def goodbye():
 def run():
     plot.plot_sine_wave()
 
-#region MAIN_EXECUTION
-print(parse.get_uncommented_text('./assets/splash.txt'))
-user_key_pressed = parse.wait_for_key_press(parse.get_uncommented_text('./assets/instructions.txt'))
-print('\n')
+if __name__ == "__main__": 
+    import plot
+    import parse
 
-user_choices = {'w': warranty, 'c': conditions, ' ': run}
+    print(parse.get_uncommented_text('./assets/splash.txt'))
+    user_key_pressed = parse.wait_for_key_press(parse.get_uncommented_text('./assets/instructions.txt'))
+    print('\n')
 
-try:
-    user_choices[user_key_pressed]()
-finally:
-    goodbye()
-#endregion
+    user_choices = {'w': warranty, 'c': conditions, ' ': run}
+
+    try:
+        user_choices[user_key_pressed]()
+    finally:
+        goodbye()
+PY
